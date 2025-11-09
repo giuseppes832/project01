de80e9240f74418b368d6b4c83a84a6ea3029fac
Giuseppe <giuseppe@example.com>
2025-11-09 03:21:25 +0100

    utente loggato

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -45,25 +45,15 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/owner-account">Account</a>
                 </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
                 @endif
+
+
                 @endauth
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout">Logout</a>
-                </li>
+
               </ul>
-              <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
+            @auth
+            {{ Auth::user()->name }}&nbsp;<a href="/logout">Logout</a>
+            @endauth
             </div>
           </div>
         </nav>
